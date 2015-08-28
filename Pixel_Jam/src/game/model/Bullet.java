5a8dbef885b44ce0483c06249ef61a15e7b3c650
petikalohi <petikalohi@sandbar.ecs.vuw.ac.nz>
2015-08-28 20:47:35 +1200

    Bullet basic functionality

--- Pixel_Jam/src/game/model/Bullet.java
+++ Pixel_Jam/src/game/model/Bullet.java
@@ -1,5 +1,48 @@
 package game.model;
 
+import java.awt.Graphics;
+
 public class Bullet {
 
+	private final int RADIUS = 5;
+
+	private int x;
+	private int y;
+
+	private double vx;
+	private double vy;
+
+	public Bullet(int xi, int yi, double direction, double speed) {
+
+
+	}
+
+	public void moveFrame() {
+
+		x += vx;
+		y += vy;
+
+		// check for collisions
+	}
+
+	public double getSpeed() {
+
+		return Math.sqrt(vx*vx + vy*vy);
+	}
+
+	public void setVelocity(double vx, double vy) {
+
+		this.vx = vx;
+		this.vy = vy;
+	}
+
+	public void setSpeed(double direction, double speed) {
+
+
+	}
+
+	public void draw(Graphics g) {
+
+		g.fillOval(x - RADIUS, y - RADIUS, RADIUS * 2, RADIUS * 2);
+	}
 }
