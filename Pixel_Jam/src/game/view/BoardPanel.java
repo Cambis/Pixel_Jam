11b3b70f703bf3804c8e162337d7004005da9920
Cameron Bryers <cambo_the_rambo@hotmail.com>
2015-08-29 13:43:46 +1200

    Started a title screen

--- Pixel_Jam/src/game/view/BoardPanel.java
+++ Pixel_Jam/src/game/view/BoardPanel.java
@@ -4,6 +4,7 @@ import game.control.Player;
 import game.model.Board;
 import game.model.Bullet;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
@@ -24,19 +25,39 @@ import javax.swing.Timer;
  */
 public class BoardPanel extends JPanel implements ActionListener {
 
+	public enum BoardState {
+		START, PLAY;
+	}
+
+	private BoardState state;
+
 	private Board board;
 
 	private Timer timer;
 
 	public BoardPanel(Board board) {
 		this.board = board;
-
+		state = BoardState.PLAY;
 		setUpTimer();
 	}
 
+	public void updateState(BoardState state) {
+		this.state = state;
+	}
+
+	public void paintTitleScreen(Graphics g) {
+		g.setColor(Color.BLACK);
+		g.drawRect(0, 0, board.getXSize() * 2 * Board.tileSize, board.getYSize() * Board.tileSize);
+	}
+
 	public void paintComponent(Graphics g) {
-		Image image = board.draw();
-		g.drawImage(image, 0, 0, null);
+		if (state.equals(BoardState.START))
+			paintTitleScreen(g);
+
+		else if (state.equals(BoardState.PLAY)) {
+			Image image = board.draw();
+			g.drawImage(image, 0, 0, null);
+		}
 	}
 
 	public static void main(String args[]) {
@@ -47,24 +68,18 @@ public class BoardPanel extends JPanel implements ActionListener {
 		frame.setVisible(true);
 	}
 
-
-
-
 	private void setUpTimer() {
 
-		timer = new Timer(1, this); //tick every 1 milliseconds
+		timer = new Timer(1, this); // tick every 1 milliseconds
 		timer.start();
 	}
 
-	//on timer tick
+	// on timer tick
 	@Override
 	public void actionPerformed(ActionEvent e) {
 
 		board.gameTick();
 		this.repaint();
 	}
-
-
-
 
 }
