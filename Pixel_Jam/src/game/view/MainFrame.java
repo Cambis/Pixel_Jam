af03b8ca4660126432b71558ee251a725f41eb9c
petikalohi <petikalohi@sandbar.ecs.vuw.ac.nz>
2015-08-30 10:02:47 +1200

    Mainframe intialises to center screen

--- Pixel_Jam/src/game/view/MainFrame.java
+++ Pixel_Jam/src/game/view/MainFrame.java
@@ -81,6 +81,8 @@ public class MainFrame extends JFrame implements KeyListener {
 		// Make sure we can sees it!
 		setVisible(true);
 
+		this.setLocationRelativeTo(null);
+
 		setResizable(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		addKeyListener(board.getPlayers().get(0));
